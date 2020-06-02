9301b6815bcdd548fa0e1276f4c265fca77e1d7a
g72446dp <dilyan.penev@student.manchester.ac.uk>
2020-06-02 14:22:25 +0300

    start page

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # DaliVali
 by **Dilyan Penev** \
-started on 31.05.2020 \
-API key: 246384c9beadbdc4eb18ced4c193e0ae
+started on 31.05.2020
--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from kivy.app import App
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.properties import ObjectProperty
+
+
+class StartPage(Screen):
+    city = ObjectProperty(None)
+
+    def submit_city(self):
+        print(self.city.text)
+
+    def delete_prompt(self):
+        if self.city.text == 'Enter city name':
+            self.city.text = ''
+
+
+class SecondPage(Screen):
+    pass
+
+
+class PageManager(ScreenManager):
+    pass
 
 
 class WeatherApp(App):
-    pass
+    def build(self):
+        return PageManager()
 
 
 if __name__ == '__main__':
