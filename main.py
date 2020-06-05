47490334d0b59a07f4040c9d2119940fcd056b7b
g72446dp <dilyan.penev@student.manchester.ac.uk>
2020-06-05 15:23:18 +0300

    recycle view and basic forecasting

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 from kivy.app import App
 from kivy.uix.screenmanager import Screen, ScreenManager
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, ListProperty
 from weather_forcaster import CitySearch
+from kivy.uix.recycleview import RecycleView
+from kivy.uix.button import Button
 
 
 class StartPage(Screen):
     city = ObjectProperty(None)
 
     def submit_city(self):
-        cs = CitySearch(self.city.text)
-        print(cs.list_of_results)
+        cs = CitySearch()
+        rv_list = cs.search_city(self.city.text)
+        self.manager.screens[1].ids.search_res.data = rv_list
 
     def delete_prompt(self):
         if self.city.text == 'Enter city name':
             self.city.text = ''
 
 
-class CitySearchPage(Screen):
+class RV(RecycleView):
+    def __init__(self, **kwargs):
+        super(RV, self).__init__(**kwargs)
+
+
+class CityButton(Button):
+    def __init__(self, **kwargs):
+        super(CityButton, self).__init__(**kwargs)
+
+    def on_release(self):
+        cs = CitySearch()
+        print(cs.get_weather_status(self.text.split(', ')))
+
+
+class RVPage(Screen):
     pass
 
 
@@ -26,7 +43,10 @@ class PageManager(ScreenManager):
 
 class WeatherApp(App):
     def build(self):
-        return PageManager()
+        root = PageManager()
+        root.add_widget(StartPage(name='start'))
+        root.add_widget(RVPage(name='rvpage'))
+        return root
 
 
 if __name__ == '__main__':
--- weather_forcaster.py
+++ weather_forcaster.py
@@ -2,9 +2,23 @@ from pyowm.owm import OWM
 
 
 class CitySearch:
+    def __init__(self):
+        self.owm = OWM('246384c9beadbdc4eb18ced4c193e0ae')
+        self.reg = self.owm.city_id_registry()
+        self.mgr = self.owm.weather_manager()
 
-    def __init__(self, search_word):
-        self.search_word = search_word
-        owm = OWM('246384c9beadbdc4eb18ced4c193e0ae')
-        reg = owm.city_id_registry()
-        self.list_of_results = reg.ids_for(self.search_word)
+    def search_city(self, search_word):
+        results = self.reg.ids_for(search_word)
+        rv_dict = [{'text': str(name + ', ' + country)} for cityid, name, country in results]
+        return rv_dict
+
+    def get_weather_status(self, city_details):
+        city_name = city_details[0]
+        country_name = city_details[1]
+        print(city_name, country_name)
+        location = self.reg.locations_for(city_name, country=country_name)
+        if isinstance(location, list):
+            location = location[0]
+        one_call = self.mgr.one_call(location.lat, location.lon)
+        # return [one_call.forecast_hourly[i].status for i in range(10)]
+        return one_call.forecast_hourly[0].status
