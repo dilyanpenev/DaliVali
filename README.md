2f2c8d5b4736a381fc1a5cb28d3eaab24c0fc581
g72446dp <dilyan.penev@student.manchester.ac.uk>
2020-06-19 22:03:45 +0300

    added logo and rain intensity

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # DaliVali
 by **Dilyan Penev** \
 started on 31.05.2020
+
+Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
--- weather_forcaster.py
+++ weather_forcaster.py
@@ -7,6 +7,43 @@ def get_remaining_hours(timezone='EEST'):
     return 24 - hour_now
 
 
+def calculate_rain_chance(status_list, rain_list, hour_range):
+    advice = 0
+    if 'Rain' in status_list:
+        next_rain = None
+        for no, status in enumerate(status_list):
+            if status == 'Rain':
+                if rain_list[no].get('1h') <= 0.5 and advice < 2:
+                    advice = 1
+                elif rain_list[no].get('1h') <= 2.5 and advice < 3:
+                    advice = 2
+                else:
+                    advice = 3
+                if next_rain is None:
+                    next_rain = no
+        if advice == 1:
+            advice = 'Nothing more than a drizzle today.'
+        elif advice == 2:
+            advice = 'You\'ll definitely need an umbrella today.'
+        else:
+            advice = 'It\'s not a good idea to go out. Heavy rain is coming.'
+        if next_rain == 0:
+            advice += ' It\'s probably raining right now.'
+        elif next_rain == 1:
+            advice += ' It\'s going to rain in 1 hour'
+        else:
+            advice += ' It\'s going to rain in ' + str(next_rain) + ' hours'
+    elif 'Clear' in status_list:
+        is_sunny = 24 - hour_range + status_list.index('Clear')
+        if 6 <= is_sunny <= 18:
+            advice = 'Sunglasses will be more useful than an umbrella today.'
+        else:
+            advice = 'Fortunately, no rain today.'
+    else:
+        advice = 'Fortunately, no rain today.'
+    return advice
+
+
 class CitySearch:
     def __init__(self):
         self.owm = OWM('246384c9beadbdc4eb18ced4c193e0ae')
@@ -25,20 +62,7 @@ class CitySearch:
         if isinstance(location, list):
             location = location[0]
         one_call = self.mgr.one_call(location.lat, location.lon)
-        rain_forecast = [one_call.forecast_hourly[i].status for i in range(hour_range)]
-        advice = 'No rain today.'
-        if 'Rain' in rain_forecast:
-            advice = 'You will definitely need an umbrella today.'
-            next_rain = rain_forecast.index('Rain')
-            if next_rain == 0:
-                advice += ' It is probably raining right now.'
-            elif next_rain == 1:
-                advice += ' It is going to rain in 1 hour'
-            else:
-                advice += ' It is going to rain in ' + str(next_rain) + ' hours'
-        else:
-            if 'Clear' in rain_forecast:
-                is_sunny = 24 - hour_range + rain_forecast.index('Clear')
-                if is_sunny >= 6 and is_sunny <= 18:
-                    advice = 'Better get sunglasses than an umbrella.'
+        status_forecast = [one_call.forecast_hourly[i].status for i in range(hour_range)]
+        rain_forecast = [one_call.forecast_hourly[i].rain for i in range(hour_range)]
+        advice = calculate_rain_chance(status_forecast, rain_forecast, hour_range)
         return advice
